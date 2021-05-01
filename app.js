a4443d7ec1f305edd8f591886871f8dd3f7e6f61
luksk <lukskill@hotmail.com>
2021-05-01 15:44:47 -0300

    multiroom_chat Socket

--- app.js
+++ app.js
@@ -2,6 +2,47 @@
 var app = require('./config/server');
 
 /**parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor online');
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+//criar a connection para o web socket
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuario desconectou');
+    });
+
+    socket.on('msgParaServidor', function(data){
+        /**dialogo */
+        socket.emit('msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+    
+        socket.broadcast.emit('msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+        
+        
+        /**participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit('participantesParaCliente', 
+            {apelido: data.apelido}
+        );
+    
+        socket.broadcast.emit('participantesParaCliente', 
+            {apelido: data.apelido}
+        );
+
+        }
+
+        
+    
+    
+    });
 });
